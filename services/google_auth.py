4a12f487da4a488e9d33b54b57125d6063e4a59b
Edris9 <Edriskohestani1010@gmail.com>
2026-03-08 03:01:46 +0100

    Environment Variables** → lägg till

--- services/google_auth.py
+++ services/google_auth.py
@@ -1,12 +1,24 @@
+import os
+import json
 from google_auth_oauthlib.flow import Flow
-from config.settings import CLIENT_SECRETS_FILE, SCOPES, REDIRECT_URI
+from config.settings import SCOPES, REDIRECT_URI
 
 def create_flow():
-    flow = Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE,
-        scopes=SCOPES,
-        redirect_uri=REDIRECT_URI
-    )
-    # Lägg till denna rad för att stänga av kravet på code_verifier
-    flow.code_challenge_method = None 
+    credentials_json = os.environ.get("GOOGLE_CREDENTIALS")
+    
+    if credentials_json:
+        client_config = json.loads(credentials_json)
+        flow = Flow.from_client_config(
+            client_config,
+            scopes=SCOPES,
+            redirect_uri=REDIRECT_URI
+        )
+    else:
+        from config.settings import CLIENT_SECRETS_FILE
+        flow = Flow.from_client_secrets_file(
+            CLIENT_SECRETS_FILE,
+            scopes=SCOPES,
+            redirect_uri=REDIRECT_URI
+        )
+    
     return flow
