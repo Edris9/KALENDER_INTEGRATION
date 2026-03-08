04c8d691c36adbe4eda3fd459137c4df81d1dae5
Edris9 <Edriskohestani1010@gmail.com>
2026-03-08 12:34:47 +0100

    uppdate setting

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,5 @@
+import os
+
 CLIENT_SECRETS_FILE = "credentials.json"
 
 SCOPES = [
@@ -5,4 +7,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/calendar.events"
 ]
 
-REDIRECT_URI = "http://127.0.0.1:5000/callback"
+REDIRECT_URI = os.environ.get(
+    "REDIRECT_URI",
+    "http://localhost:5000/callback"
+)
