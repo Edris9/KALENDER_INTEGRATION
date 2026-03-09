2cf7a6b7734fd1f2610687c58fe6dc07bf9532f3
Edris9 <Edriskohestani1010@gmail.com>
2026-03-09 11:23:59 +0100

    Fix supabase_client filename case

--- services/Supabase_Client.py
+++
@@ -1,7 +0,0 @@
-import os
-from supabase import create_client
-
-SUPABASE_URL = os.environ.get("SUPABASE_URL", "https://nyhxkzjcxhljtzrwujde.supabase.co")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55aHhrempjeGhsanR6cnd1amRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMDY1NTYsImV4cCI6MjA4ODU4MjU1Nn0.MR0CzPl8nQiADYmFrBr7WNDh7vU_Thyvq8Nm5ilPwGE")
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
